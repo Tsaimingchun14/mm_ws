c09138ed7f2fb7c4d6341eefc9d14bde6a98c2df
Tsaimingchun14 <jerry110030014@gapp.nthu.edu.tw>
2026-02-04 15:33:44 +0800

    add camera launch

--- src/motion_planner/setup.py
+++ src/motion_planner/setup.py
@@ -10,6 +10,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', [
+            'launch/d435i_high_resolution.launch.py',
+            'launch/d435i_basic.launch.py',
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
